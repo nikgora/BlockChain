cf3ac2d1e7e02564f19e4e94e0e89e9384c0d9dd
nikgora <nik.gora.2004@gmail.com>
2022-05-20 10:56:13 +0200

    Add files via upload

---
+++ Prac1/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        BigInteger numbersOfBits ,base = new BigInteger("2");
+        Scanner in = new Scanner(System.in);
+        var input = in.next();
+        numbersOfBits = new BigInteger(input);
+        BigInteger rand = MyRandom(numbersOfBits.intValue());
+        System.out.println(MyPow(base,numbersOfBits));
+        System.out.println(rand);
+        System.out.print(MyTime(rand));
+
+    }
+    public static BigInteger MyRandom(int numbersOfBits) {
+        return new BigInteger(numbersOfBits, new Random());
+    }
+
+    public static BigInteger MyPow( BigInteger base,BigInteger numbersOfBits)
+    {
+        return base.pow(numbersOfBits.intValue());
+    }
+
+    public static long MyTime(BigInteger random) {
+        long time = System.currentTimeMillis();
+        BigInteger brutForce = new BigInteger("0");
+        while (!brutForce.equals(random))
+        {
+            brutForce=brutForce.add(new BigInteger("1"));
+        }
+        return System.currentTimeMillis()-time;
+    }
+
+}
